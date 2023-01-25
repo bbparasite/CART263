c17637ef0c729c8ad908ab6aed863a24d5f2ebd5
Jaden Thompson <95893012+bbparasite@users.noreply.github.com>
2023-01-25 15:20:03 -0500

    Example framework
    Following some of the reading form Nature of Code and examining the example provided on p5js

--- Project 1/Particle System/js/script.js
+++ Project 1/Particle System/js/script.js
@@ -1,33 +1,76 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+/*
+Particles With Personality - "Needs a bit of help to get going"
+Author Name: Jaden Thompson
 */
 
 "use strict";
+let system;
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
+/*
 Description of setup
 */
 function setup() {
-
+    createCanvas(500, 500);
+    system = new ParticleSystem(createVector(width / 2, 50));
 }
 
-
-/**
+/*
 Description of draw()
 */
 function draw() {
+    background(51);
+    system.addParticle();
+    system.run();
+}
 
+class Particle {
+    constructor(position) {
+        this.acceleration = createVector(0, 0.1);
+        this.velocity = createVector(random(-1, 1), random(-1, 0));
+        this.position = position.copy();
+        this.lifespan = 255;
+        this.state = 0;
+    }
+
+    run() {
+        this.update();
+        this.display();  
+    }
+
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.lifespan -= 2;
+    }
+
+    display() {
+        stroke(200, this.lifespan);
+        fill(127, this.lifespan);
+        ellipse(this.position.x, this.position.y, 12, 12);
+    }  
+
+    isDead() {
+        return this.lifespan < 0;
+    }
+}
+
+class ParticleSystem {
+    constructor(position) {
+        this.origin = position.copy();
+        this.particles = [];
+    }
+    
+    addParticle() {
+        this.particles.push(new Particle(this.origin));
+    }
+
+    run() {
+        for (let i = this.particles.length-1; i >= 0; i--) {
+            let p = this.particles[i];
+            p.run();
+            if (p.isDead()) {
+              this.particles.splice(i, 1);
+            }
+        }
+    }
 }
