99d3992183bcb5113152d054c08c0ef2a8ceee8b
Jaden Thompson <95893012+bbparasite@users.noreply.github.com>
2023-03-08 17:54:14 -0500

    Added 3D library for text and Music

--- Project 2/js/script.js
+++ Project 2/js/script.js
@@ -1,14 +1,20 @@
 let table;
+let song;
 let points = [];
+let wordObject;
 
 function preload() {
   table = loadTable("assets/archive/2019.csv", "csv", "header");
+  song = loadSound("assets/sounds/NIN.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth + 50, windowHeight + 50, WEBGL);
   background(0);
-  fill(255);
+  userStartAudio();
+  if (song != song.isPlaying()){
+    song.play();
+  }
   console.log(table.getRowCount() + " total rows in table");   
   console.log(table.getColumnCount() + " total columns in table"); 
   for (var r = 0; r < table.getRowCount(); r++){ 
@@ -22,11 +28,23 @@ function setup() {
                                 table.getString(r, 3), 
                                 table.getString(r, 0));
                                 // Pass through the values in each row
-      // points[r].drawCircle();
   }
+  wordObject = createWord3D(
+    string = "Are you happy?",       // The actual character that you want to draw (anything that can be passed into "text()")
+    depth = 10,        // How thick the 3D rendered letter is (i.e. how many cube pixels of size "size" it is on z-axis)  
+    size = 1,         // The size of a unit "box()" making up part of the letter  
+    resolution = 20,   // The size of the canvas it renders the letter on (higher is more detailed, 20-30 is a good range)  
+    bevelled = false,     // [OPTIONAL, default = true] Gives the bevelled, embossed 3D look (as seen in screenshot)  
+    font = "Georgia",         // [OPTIONAL, default = "Georgia"] Gives the font uses, can be any default ones or anything added  
+    style = "NORMAL"         // [OPTIONAL, default = BOLD] Gives the chosen style out of BOLD, NORMAL, ITALIC  
+  )
 }
 
 function draw() {
+  push();
+  translate(-600, -300, 0);
+  wordObject.show();
+  pop();
   for (var i = 0; i < table.getRowCount(); i++){
     points[i].drawHappy();
   }
@@ -45,7 +63,6 @@ class DataPoint {
 
     drawHappy(){
       rotateZ(millis() / 200000 * this.score);
-
       push();
       fill(255, map(this.score, 1, 7, 0, 255), 0);
       translate(0, -100, map(this.score, 1, 7.2, -10000, 2000));
