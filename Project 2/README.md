07e8ec2ade388c8f7b19fae0e16f36fc730ebb50
Jaden Thompson <95893012+bbparasite@users.noreply.github.com>
2023-03-09 22:42:34 -0500

    Final adjustments
    Tweaking the speeds, including new text, adding comments, and updating the README

--- Project 2/README.md
+++ Project 2/README.md
@@ -1,4 +1,12 @@
 # Project 2 - Data Visualization
 
 ## Overview
-Create a piece of code that visualizes any data set in a meaningful way. This can be a piece of live data or information taken from an API, or it can be a data set you have downloaded. Be creative, try and get away from basic charts or maps and represent your data in an interesting, informative, and unique way. Pick a data set that is meaningful to you, and share that information with your audience
+Create a piece of code that visualizes any data set in a meaningful way. This can be a piece of live data or information taken from an API, or it can be a data set you have downloaded. Be creative, try and get away from basic charts or maps and represent your data in an interesting, informative, and unique way. Pick a data set that is meaningful to you, and share that information with your audience.
+
+## Conception & Implementation
+
+Conceptually I wanted my visualization to come across more ambiguous and less representational in the realistic sense of data visualization. Each torus represents a different country with their colour and speed being directly influenced by where they score on the world happiness index. Directly pulling from the project details the notion of straying away from traditional charts or maps to represent it in an interesting, informative, and unique way. I wanted the viewer to have their own interpretation on the piece that I created without simply giving all the information directly to the viewer, only fully understanding the idea after it was explained to them. 
+
+I played around with the idea of incorporating a better signifer for the data points represented within my code by creating text objects for each country but due to 3D rendering constraints it caused my file to completely stall and almost crash my web browser.
+
+The main inspiration for this project was centered around the aesthetics of weirdcore.
--- Project 2/js/script.js
+++ Project 2/js/script.js
@@ -1,15 +1,19 @@
+// Global variables for loading and creating objects
 let table;
 let song;
 let points = [];
-let wordObject = [];
+let happyObject = [];
+let stressObject = [];
 
+// Loading in the dataset and music
 function preload() {
   table = loadTable("assets/archive/2019.csv", "csv", "header");
   song = loadSound("assets/sounds/NIN.mp3");
 }
 
+// Traditional setup function with creating the canvas size, background, audio, data points, and Word3D objects
 function setup() {
-  createCanvas(1795, 931, WEBGL);
+  createCanvas(1795, 931, WEBGL); // Converted the canvas to the WEBGL renderer
   background(0);
   userStartAudio();
   if (song != song.isPlaying()){
@@ -17,18 +21,13 @@ function setup() {
   }
   console.log(table.getRowCount() + " total rows in table");   
   console.log(table.getColumnCount() + " total columns in table"); 
-  for (var r = 0; r < table.getRowCount(); r++){ 
-    for (var c = 0; c < table.getColumnCount(); c++){       
-      console.log(table.getString(r, c));
-    }
-  } 
   for (var r = 0; r < table.getRowCount(); r++){ // Cycle through each row of the table
       points[r] = new DataPoint(table.getString(r, 1), 
                                 table.getString(r, 2));
                                 // Pass through the values in each row
   }
-  for(var i = 0; i < 12; i++){
-  wordObject[i] = createWord3D(
+  for(var i = 0; i < 6; i++){ // Creating 6 happy text objects
+  happyObject[i] = createWord3D(
       string = "Are you happy?",       // The actual character that you want to draw (anything that can be passed into "text()")
       depth = 10,        // How thick the 3D rendered letter is (i.e. how many cube pixels of size "size" it is on z-axis)  
       size = 1,         // The size of a unit "box()" making up part of the letter  
@@ -36,10 +35,22 @@ function setup() {
       bevelled = false,     // [OPTIONAL, default = true] Gives the bevelled, embossed 3D look (as seen in screenshot)  
       font = "Georgia",         // [OPTIONAL, default = "Georgia"] Gives the font uses, can be any default ones or anything added  
       style = "NORMAL"         // [OPTIONAL, default = BOLD] Gives the chosen style out of BOLD, NORMAL, ITALIC  
-    )
+    );
   }
+  for(var i = 0; i < 6; i++){ // Creating 6 stress text objects
+    stressObject[i] = createWord3D(
+        string = "Are you stressed?",       // The actual character that you want to draw (anything that can be passed into "text()")
+        depth = 10,        // How thick the 3D rendered letter is (i.e. how many cube pixels of size "size" it is on z-axis)  
+        size = 1,         // The size of a unit "box()" making up part of the letter  
+        resolution = 20,   // The size of the canvas it renders the letter on (higher is more detailed, 20-30 is a good range)  
+        bevelled = false,     // [OPTIONAL, default = true] Gives the bevelled, embossed 3D look (as seen in screenshot)  
+        font = "Georgia",         // [OPTIONAL, default = "Georgia"] Gives the font uses, can be any default ones or anything added  
+        style = "NORMAL"         // [OPTIONAL, default = BOLD] Gives the chosen style out of BOLD, NORMAL, ITALIC  
+      );
+    }
 }
 
+// Using the draw funtion to display the data points and text objects
 function draw() {
   push();
   for (var i = 0; i < table.getRowCount(); i++){
@@ -49,13 +60,13 @@ function draw() {
   for(var i = 0; i < 6; i++){
     push();
     translate(-600, -300 + (100 * i), 0);
-    wordObject[i].show();
+    happyObject[i].show();
     pop();
   }
-  for(var i = 6; i < 12; i++){
+  for(var i = 0; i < 6; i++){
     push();
-    translate(600, -900 + (100 * i), 0);
-    wordObject[i].show();
+    translate(600, -300 + (100 * i), 0);
+    stressObject[i].show();
     pop();
   }
 }
@@ -67,12 +78,12 @@ class DataPoint {
         this.score = score;
     }
 
+    // This is the method used for drawing each torus shape
     drawHappy(){
-      rotateZ(millis() / 200000 * this.score);
+      rotateZ(millis() / 150000 * this.score); // Rotating all of them along the Z axis
       push();
-      // noStroke();
-      fill(255, map(this.score, 1, 7, 0, 255), 0);
-      translate(0, -100, map(this.score, 1, 7.2, -10000, 2000) + (millis() / 600));
+      fill(255, map(this.score, 4, 7, 0, 255), 0); // Filling each data point based on their happiness index
+      translate(0, -100, map(this.score, 1, 7.2, -10000, 2000)+ (millis() / 100)); // Mapping each data point based on their happiness index and moving them towards the viewer
       torus(30, 15);
       pop();
     }
